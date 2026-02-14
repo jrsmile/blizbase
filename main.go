2b602b69f3e8a31d2a1c65af07dc0a73af9e04f1
JRSmile <jan@behead.de>
2026-02-14 22:48:42 +0100

    added fallback if no .env file

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ import (
 func goDotEnvVariable(key string) string {
 	err := godotenv.Load(".env")
 	if err != nil {
-		log.Fatalf("Error loading .env file")
+		log.Printf("Error loading .env file, falling back to environment variables: %v", err)
 	}
 	return os.Getenv(key)
 }
